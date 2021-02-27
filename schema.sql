d42bbb250f9de5e072a395319cdbeebc592c75d4
Faster Speeding <luke@lmbyrne.dev>
2021-02-27 21:12:50 +0000

    Reformat schema.sql

--- schema.sql
+++ schema.sql
@@ -30,73 +30,112 @@
 -- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 -- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 CREATE TABLE IF NOT EXISTS users (
-    id            BIGINT GENERATED ALWAYS AS IDENTITY,
-    created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    flags         BIGINT                   NOT NULL,
-    password_hash VARCHAR                  NOT NULL, -- argon2  -- TODO: bytea?
-    username      VARCHAR(32)              NOT NULL, -- TODO: case insensitivity?
+    id              BIGINT                      GENERATED ALWAYS AS IDENTITY,
+    created_at      TIMESTAMP WITH TIME ZONE    NOT NULL                        DEFAULT CURRENT_TIMESTAMP,
+    flags           BIGINT                      NOT NULL,
+    password_hash   VARCHAR                     NOT NULL,                       -- argon2  -- TODO: bytea?
+    username        VARCHAR(32)                 NOT NULL,                       -- TODO: case insensitivity?
 
-    CONSTRAINT user_pk PRIMARY KEY (id),
-    CONSTRAINT user_username_uc UNIQUE (username)
+    CONSTRAINT user_pk
+        PRIMARY KEY (id),
+
+    CONSTRAINT user_username_uc
+        UNIQUE (username)
 );
 
 
 CREATE TABLE IF NOT EXISTS devices (
-    id                 BIGINT GENERATED ALWAYS AS IDENTITY,
-    access             INT         NOT NULL,
-    is_required_viewer BOOLEAN     NOT NULL, -- TODO: This might be backwards
-    name               VARCHAR(32) NOT NULL,
-    user_id            BIGINT      NOT NULL,
+    id                  BIGINT      GENERATED ALWAYS AS IDENTITY,
+    access              INT         NOT NULL,
+    is_required_viewer  BOOLEAN     NOT NULL,                       -- TODO: This might be backwards
+    name                VARCHAR(32) NOT NULL,
+    user_id             BIGINT      NOT NULL,
 
-    CONSTRAINT device_pk PRIMARY KEY (id),
-    CONSTRAINT device_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT device_uc UNIQUE (name, user_id)
+    CONSTRAINT device_pk
+        PRIMARY KEY (id),
+
+    CONSTRAINT device_user_id_fk
+        FOREIGN KEY (user_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT device_uc
+        UNIQUE (name, user_id)
 );
 
 
+ -- TODO: Find a better name for if this should delete after being viewed than "is_transient"
 CREATE TABLE IF NOT EXISTS messages (
-    id           BIGINT GENERATED ALWAYS AS IDENTITY,
-    created_at   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    expire_at    TIMESTAMP WITH TIME ZONE,
-    is_public    BOOLEAN                  NOT NULL DEFAULT False,
-    is_transient BOOLEAN                  NOT NULL, -- Find a better name for if this should delete after being viewed
-    text         VARCHAR,                           -- TODO: not nullable?
-    title        VARCHAR,                           -- TODO: not nullable?
-    user_id      BIGINT                   NOT NULL,
+    id              BIGINT                      GENERATED ALWAYS AS IDENTITY,
+    created_at      TIMESTAMP WITH TIME ZONE    NOT NULL                        DEFAULT CURRENT_TIMESTAMP,
+    expire_at       TIMESTAMP WITH TIME ZONE,
+    is_public       BOOLEAN                     NOT NULL                        DEFAULT False,
+    is_transient    BOOLEAN                     NOT NULL,
+    text            VARCHAR,                    -- TODO: not nullable?
+    title           VARCHAR,                    -- TODO: not nullable?
+    user_id         BIGINT                      NOT NULL,
 
-    CONSTRAINT message_pk PRIMARY KEY (id),
-    CONSTRAINT message_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    CONSTRAINT message_pk
+        PRIMARY KEY (id),
+
+    CONSTRAINT message_user_id_fk
+        FOREIGN KEY (user_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE
 );
 
 
 -- The actual files for deleted files will be deleted by a cron-job running on the file service at one point.
 CREATE TABLE IF NOT EXISTS files (
-    id         BIGINT GENERATED ALWAYS AS IDENTITY,
-    file_name  VARCHAR NOT NULL,
-    message_id BIGINT  NOT NULL,
+    id          BIGINT      GENERATED ALWAYS AS IDENTITY,
+    file_name   VARCHAR     NOT NULL,
+    message_id  BIGINT      NOT NULL,
 
-    CONSTRAINT file_pk PRIMARY KEY (id),
-    CONSTRAINT files_message_id_fk FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE
+    CONSTRAINT file_pk
+        PRIMARY KEY (id),
+
+    CONSTRAINT files_message_id_fk
+        FOREIGN KEY (message_id)
+        REFERENCES messages (id)
+        ON DELETE CASCADE
 );
 
 
 CREATE TABLE IF NOT EXISTS permissions (
-    message_id  BIGINT NOT NULL,
-    permissions BIGINT NOT NULL,
-    user_id     BIGINT NOT NULL,
+    message_id  BIGINT  NOT NULL,
+    permissions BIGINT  NOT NULL,
+    user_id     BIGINT  NOT NULL,
 
-    CONSTRAINT permission_pk PRIMARY KEY (message_id, user_id),
-    CONSTRAINT permission_message_id_fk FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE,
-    CONSTRAINT permission_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    CONSTRAINT permission_pk
+        PRIMARY KEY (message_id, user_id),
+
+    CONSTRAINT permission_message_id_fk
+        FOREIGN KEY (message_id)
+        REFERENCES messages (id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT permission_user_id_fk
+        FOREIGN KEY (user_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE
 );
 
 
 CREATE TABLE IF NOT EXISTS views (
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    device_id  BIGINT                   NOT NULL,
-    message_id BIGINT                   NOT NULL,
+    created_at  TIMESTAMP WITH TIME ZONE    NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    device_id   BIGINT                      NOT NULL,
+    message_id  BIGINT                      NOT NULL,
 
-    CONSTRAINT view_pk PRIMARY KEY (device_id, message_id),
-    CONSTRAINT views_device_id_fk FOREIGN KEY (device_id) REFERENCES devices (id) ON DELETE CASCADE,
-    CONSTRAINT views_message_id_fk FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE
+    CONSTRAINT view_pk
+        PRIMARY KEY (device_id, message_id),
+
+    CONSTRAINT views_device_id_fk
+        FOREIGN KEY (device_id)
+        REFERENCES devices (id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT views_message_id_fk
+        FOREIGN KEY (message_id)
+        REFERENCES messages (id)
+        ON DELETE CASCADE
 );
